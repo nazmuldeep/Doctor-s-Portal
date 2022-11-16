f0580881b43ce1b308c0950d871ff59fcae224fb
nazmuldip <nazmuldip21@gmail.com>
2022-11-16 16:55:32 +0600

    Organize folder and home page design done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Banner from './Banner/Banner';
+import InfoCards from './InfoCards/InfoCards';
+import MakeAppoinment from './MakeAppoinment/MakeAppoinment';
+import Services from './Services/Services';
+import Testimonial from './Testimonial/Testimonial';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='mx-5'>
+            <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <MakeAppoinment></MakeAppoinment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
 
-export default Home; <h2>This is home</h2>
+export default Home;
